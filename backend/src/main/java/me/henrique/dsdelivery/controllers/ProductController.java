ef0e6b9fe503d123f8915c3e944157a54f218469
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-01-05 16:29:45 -0300

    GET /products

---
+++ backend/src/main/java/me/henrique/dsdelivery/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package me.henrique.dsdelivery.controllers;
+
+import me.henrique.dsdelivery.dto.ProductDTO;
+import me.henrique.dsdelivery.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> findAll() {
+        List<ProductDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
