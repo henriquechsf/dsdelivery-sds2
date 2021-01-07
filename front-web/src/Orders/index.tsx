8f2b80486adeaaffd79c1e97a783bb75596471f9
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-01-07 00:24:54 -0300

    listagem dos cards de produtos dinamicamente

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,28 @@
-import ProductsList from './ProductsList';
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
+
 import './styles.css';
 
+import ProductsList from './ProductsList';
+import StepsHeader from './StepsHeader';
+
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products)
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
-            <ProductsList />
+            <ProductsList products={products} />
         </div>
     )
 }
